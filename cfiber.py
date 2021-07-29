fba8ec0dbd5ed5afe4eb804e4f26c2a05c37bc22
Alina <sulemanovaaa@icloud.com>
2021-07-29 17:58:33 +0300

    fixed section list in c-fiber

--- cfiber.py
+++ cfiber.py
@@ -1,7 +1,7 @@
 from neuron import h, gui
 import math
 import random
-# h.load_mechanisms("./mod")
+#neuron.load_mechanisms("./mod")
 
 class cfiber(object):
     '''
@@ -25,13 +25,14 @@ class cfiber(object):
     recs: list
         list of receptors mechanisms (NEURON staff)
     '''
-    def __init__(self, L, d, zpozition, x_application, fast_diff, numofmodel):
+    def __init__(self, L, d, zpozition, x_application, fast_diff, numofmodel, temperature = 37):
         self.coordinates = dict()
         self.distances = dict()
         self.diffusions = dict()
         self.fast_diff = fast_diff
         self.diffs = []
         self.recs = []
+        self.synlistinh = []
         self.L = L
         self.diam = d
         self.zpozition = zpozition
@@ -43,17 +44,25 @@ class cfiber(object):
             self.num = 120
         self.create_sections()
         self.build_topology()
-        self.build_subsets()
+        # self.build_subsets()
         self.define_geometry()
         self.position()
         self.distance()
-        self.define_biophysics()
+        self.define_biophysics(temperature)
+        self.synapses()
+
     def create_sections(self):
         '''
         Creates sections (compartments)
         '''
         self.branch = h.Section(name='branch', cell=self)
         self.stimsec = [h.Section(name='stimsec[%d]' % i) for i in range(self.num)]
+        self.all_secs = h.SectionList()
+        # for sec in self.branch:
+        self.all_secs.append(sec=self.branch)
+        for sec in self.stimsec:
+            self.all_secs.append(sec=sec)
+
     def build_topology(self):
         '''
         Connects sections
@@ -71,7 +80,6 @@ class cfiber(object):
         else:
             for i in range(1, len(self.stimsec)):
                 self.stimsec[i].connect(self.stimsec[i-1])
-
     def define_geometry(self):
         '''
         Adds length and diameter to sections
@@ -79,12 +87,11 @@ class cfiber(object):
         for sec in self.stimsec:
             sec.L = self.L# microns
             sec.diam = self.diam # microns
-            sec.nseg = 4
+            sec.nseg = 3
         self.branch.L = self.L
         self.branch.diam = self.diam
-        self.branch.nseg = 3
+        self.branch.nseg = 1
         h.define_shape() # Translate into 3D points.
-
     def position(self):
         '''
         Adds 3D position
@@ -115,14 +122,13 @@ class cfiber(object):
                 self.coordinates.update({self.stimsec[i]: xyz})
         else:
             i = 0
-            for sec in self.all:
+            for sec in self.all_secs:
               h.pt3dclear()
               h.pt3dadd(self.L*i, 0, self.zpozition, self.diam)
               h.pt3dadd(self.L*(i+1), 0, self.zpozition, self.diam)
               xyz = dict(x=self.L*(i+1), y=0, z=0)
               self.coordinates.update({sec: xyz})
               i+=1
-
     def distance(self):
         '''
         Adds distances from application for every compartment
@@ -130,74 +136,83 @@ class cfiber(object):
         #self.distances.clear()
         if self.numofmodel == 11 or self.numofmodel == 12:
             self.x_application = -400
-            for compartment in self.all:
+            for compartment in self.all_secs:
                 distance = math.sqrt((30050-self.coordinates.get(compartment).get('x'))**2 + (self.x_application-self.coordinates.get(compartment).get('y'))**2 + (0.01-self.coordinates.get(compartment).get('z'))**2)
                 self.distances.update({compartment: distance})
         else:
-            for compartment in self.all:
-                distance = math.sqrt((self.x_application-self.coordinates.get(compartment).get('x'))**2 + (50-self.coordinates.get(compartment).get('y'))**2 + (0.01-self.coordinates.get(compartment).get('z'))**2)
+            for compartment in self.all_secs:
+                distance = math.sqrt((self.x_application-self.coordinates.get(compartment).get('x'))**2 + (10-self.coordinates.get(compartment).get('y'))**2 + (0.01-self.coordinates.get(compartment).get('z'))**2)
                 self.distances.update({compartment: distance})
 
-    def define_biophysics(self):
+    def define_biophysics(self, temperature):
         '''
         Adds channels and their parameters
         '''
-        for sec in self.all: # 'all' defined in build_subsets
+        for sec in self.all_secs: # 'all' defined in build_subsets
             sec.Ra = 35  # Axial resistance in Ohm * cm
             sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
             sec.insert('nav1p8')
+            sec.insert('nav1p9')
             sec.insert('extrapump')
             sec.insert('koi')
             sec.insert('naoi')
             sec.insert('nakpump')
             sec.insert('nattxs')
-            sec.insert('nav1p9')
             sec.insert('kdr')
-            sec.insert('kv1')
-            sec.insert('kv2')
-            sec.insert('kv4')
+            sec.insert('kad')
+            sec.insert('kap')
             sec.insert('leak')
-            # sec.insert('nav11_L263V')
             sec.insert('Nav1_3')
             sec.insert('extracellular')
             sec.insert('iKCa')
-            sec.insert('iCaL')
+            sec.insert('iCaAN')
             sec.insert('CaIntraCellDyn')
             if self.numofmodel == 8 or self.numofmodel >= 11:
-                sec.gbar_nav1p8= 0.22
-                sec.gbar_nav1p9 = 0.0004
+                sec.gbar_nav1p8 = 0.15
+                sec.gbar_nav1p9 = 0.00003
             elif self.numofmodel == 7:
                 sec.gbar_nav1p8 = 0.1
+                sec.gbar_nav1p9 = 0.0001
             else:
                 sec.gbar_nav1p8 = 0
+                sec.gbar_nav1p9 = 0
             sec.gbar_kdr = 0.01
-            sec.gkbar_kv1 = 0.002
-            sec.gkbar_kv2 = 0.01
-            sec.gkbar_kv4 = 0.02
+            sec.gbar_kad = 0.006
+            sec.gbar_kap = 0.006
             if self.numofmodel == 6:
                 sec.gbar_nattxs = 0.2
             else:
-                sec.gbar_nattxs = 0.2
-            sec.gbar_Nav1_3 = 0.0
-            sec.gbar_nav1p9 = 0.0#001
+                sec.gbar_nattxs = 0.1
+            sec.gbar_Nav1_3 = 0.0#2
             sec.smalla_nakpump = -0.0047891
             sec.theta_naoi = 0.029
             sec.theta_koi = 0.029
-            sec.celsiusT_nattxs = 37
-            sec.celsiusT_nav1p8= 37
-            sec.celsiusT_nakpump = 37
-            sec.gbar_iKCa = 0.0001
-            # sec.gnabar_nav11_L263V = 0.5
+            sec.celsiusT_nattxs = temperature
+            sec.celsiusT_nav1p8= temperature
+            sec.celsiusT_nav1p9= temperature
+            sec.celsiusT_nakpump = temperature
+            sec.celsiusT_kad = temperature
+            sec.celsiusT_kap = temperature
+            sec.celsiusT_kdr = temperature
+            sec.celsiusT_iKCa = temperature
+            sec.gbar_iKCa = 0.001
+            # # sec.gnabar_nav11_L263V = 0.5
             sec.depth_CaIntraCellDyn = 0.1
             sec.cai_tau_CaIntraCellDyn = 2.0
             sec.cai_inf_CaIntraCellDyn = 50.0e-6
-            sec.pcabar_iCaL = 0.00001
+            sec.gbar_iCaAN = 0.0035
+            sec.celsiusT_iCaAN = temperature
+
+
         for sec in self.stimsec:
             if self.numofmodel == 13 or self.numofmodel == 14:
-                self.add_5HTreceptors(sec, 10, 1)
+                self.add_5HTreceptors(sec, random.randint(10, 20), 10)
             else:
-                self.add_P2Xreceptors(sec, 10, 50)
-    def add_P2Xreceptors(self, compartment, time, g):
+                self.add_P2Xreceptors(sec, 10, 5, temperature)
+                self.add_5HTreceptors(sec, 10, 10)
+
+
+    def add_P2Xreceptors(self, compartment, time, g, temperature):
         '''
         Adds P2X3 receptors
         Parameters
@@ -212,7 +227,7 @@ class cfiber(object):
             receptor conductance
         '''
         if self.fast_diff:
-            diff = h.AtP_42(compartment(0.1))
+            diff = h.AtP_42(compartment(0.5))
             diff.h = self.distances.get(compartment)
             diff.tx1 = time
             diff.Deff = 0.8
@@ -225,15 +240,16 @@ class cfiber(object):
             elif self.numofmodel == 3:
                 diff.k = 0.6
             else:
-                diff.k = 0.01
+                diff.k = 0.001
         else:
             diff = h.AtP_slow(compartment(0.5))
             diff.h = self.distances.get(compartment)
             diff.tx1 = time + 0 + (diff.h/1250)*1000
             diff.c0cleft = 100
         self.diffusions.update({diff: compartment})
-        rec = h.p2x3(compartment(0.1))
+        rec = h.p2x3(compartment(0.5))
         rec.gmax = g
+        rec.celsiusT = temperature
         rec.Ev = 5
         if self.numofmodel == 4 or self.numofmodel == 5:
             rec2 = h.p2x2(compartment(0.5))
@@ -249,6 +265,7 @@ class cfiber(object):
         h.setpointer(diff._ref_atp, 'patp', rec)
         self.recs.append(rec)
         self.diffs.append(diff)
+
     def add_5HTreceptors(self, compartment, time, g):
         '''
         Adds 5HT receptors
@@ -264,7 +281,7 @@ class cfiber(object):
             receptor conductance
         '''
         if self.fast_diff:
-            diff = h.diff_5HT(compartment(0.25))
+            diff = h.diff_5HT(compartment(0.5))
             diff.h = self.distances.get(compartment)
             diff.tx1 = time
             if self.numofmodel == 14:
@@ -283,14 +300,15 @@ class cfiber(object):
         h.setpointer(diff._ref_serotonin, 'serotonin', rec)
         self.diffs.append(diff)
         self.recs.append(rec)
+
     def build_subsets(self):
         '''
         adds sections in NEURON SectionList
         '''
         self.all = h.SectionList()
-        for sec in self.stimsec:
+        for sec in h.allsec():
           self.all.append(sec=sec)
-        self.all.append(sec=self.branch)
+
     def connect2target(self, target):
         '''
         Adds presynapses
@@ -306,3 +324,10 @@ class cfiber(object):
         nc = h.NetCon(self.branch(1)._ref_v, target, sec = self.branch)
         nc.threshold = 10
         return nc
+    def synapses(self):
+        '''
+        Adds synapses
+        '''
+        for i in range(10):
+            s = h.GABAa_DynSyn(self.branch(0.5)) # Inhibitory
+            self.synlistinh.append(s)
--- pain_circuit.py
+++ pain_circuit.py
@@ -6,13 +6,18 @@ import math
 #neuron.load_mechanisms("./mod")
 from adelta import adelta
 from cfiber import cfiber
+import random
 
 from adelta2 import adelta2
 from interneuron import interneuron
 from interneuron import interneuron
 from WDR_neuron import WDR_model
 from HT_neuron import HT_model
+import numpy as np
 
+recs = []
+diffs = []
+gaba_ncs = []
 
 def set_recording_vectors(compartment):
     ''' recording voltage
@@ -60,18 +65,18 @@ def balance(cell, vinit=-55):
     vinit: int (mV)
         initialized voltage
     '''
-    for sec in cell.all:
+    for sec in cell.all_secs:
         if ((-(sec.ina_nattxs + sec.ina_nav1p8 + sec.ina_Nav1_3  + sec.ina_nakpump + sec.ina_nav1p9) / (vinit - sec.ena)) < 0):
             sec.pumpina_extrapump = -(sec.ina_nattxs + sec.ina_nav1p8  + sec.ina_Nav1_3 + sec.ina_nakpump + sec.ina_nav1p9)
         else:
             sec.gnaleak_leak = -(sec.ina_nattxs + sec.ina_nav1p8  + sec.ina_Nav1_3 + sec.ina_nakpump + sec.ina_nav1p9) / (vinit - sec.ena)
 
-        if ((-(sec.ik_kdr + sec.ik_nakpump + sec.ik_kv1 + sec.ik_kv4 + sec.ik_kv2 + sec.ik_iKCa) / (vinit - sec.ek)) < 0):
-            sec.pumpik_extrapump = -(sec.ik_kdr + sec.ik_nakpump + sec.ik_kv1 + sec.ik_kv4 + sec.ik_kv2 + sec.ik_iKCa)
+        if ((-(sec.ik_kdr + sec.ik_nakpump + sec.ik_kap + sec.ik_kad + sec.ik_iKCa) / (vinit - sec.ek)) < 0):
+            sec.pumpik_extrapump = -(sec.ik_kdr + sec.ik_nakpump + sec.ik_kad + sec.ik_kap+ sec.ik_iKCa)
         else:
-            sec.gkleak_leak = -(sec.ik_kdr + sec.ik_nakpump + sec.ik_kv1 + sec.ik_kv4 + sec.ik_kv2 + sec.ik_iKCa) / (vinit - sec.ek)
+            sec.gkleak_leak = -(sec.ik_kdr + sec.ik_nakpump + sec.ik_kap + sec.ik_kad  + sec.ik_iKCa) / (vinit - sec.ek)
 
-def simulate(cell, tstop=500, vinit=-60):
+def simulate(cells, tstop=500, vinit=-55):
     ''' simulation control
     Parameters
     ----------
@@ -83,7 +88,8 @@ def simulate(cell, tstop=500, vinit=-60):
         initialized voltage
     '''
     h.finitialize(vinit)
-    balance(cell)
+    for cell in cells:
+        balance(cell)
     if h.cvode.active():
         h.cvode.active()
     else:
@@ -92,6 +98,35 @@ def simulate(cell, tstop=500, vinit=-60):
     h.tstop = tstop
     h.v_init = vinit
     h.run()
+
+def spike_time_rec(section):
+    vec = h.Vector()
+    netcon = h.NetCon(section(0.5)._ref_v, None, sec=section)
+    netcon.record(vec)
+    return vec
+
+
+def add_gaba_receptors(synapses, pre, g):
+
+    for syn in synapses:
+        nc = h.NetCon(pre(1)._ref_v, syn, sec = pre)
+        nc.threshold = 0
+        nc.delay = random.gauss(1, 0.2)
+        nc.weight[0] = random.gauss(g, g / 10)
+        gaba_ncs.append(nc)
+
+def add_5HT_receptors(compartment, dist, g):
+    for sec in compartment:
+        diff = h.slow_5HT(sec(0.5))
+        diff.h = dist#self.distances.get(compartment)
+        diff.tx1 = 0 #+ (diff.h/50)*10#00
+        diff.c0cleft = 2
+        diff.a = 30
+        rec = h.r5ht3a(sec(0.5))
+        rec.gmax = random.gauss(g, g / 10)
+        h.setpointer(diff._ref_serotonin, 'serotonin', rec)
+        diffs.append(diff)
+        recs.append(rec)
 
 def add_glu_receptors(compartment, pre, g1, g2):
     '''
@@ -107,19 +142,19 @@ def add_glu_receptors(compartment, pre, g1, g2):
     g: float
         receptor conductance
     '''
-    recs =[]
-    diffs = []
     diff = h.GrC_Gludif3(pre(0.5))
     h.setpointer(pre(0.5)._ref_v, 'affv', diff)
     diffs.append(diff)
 
     for sec in compartment:
+        # print(compartment)
         rec = h.ampa_rec(sec(0.5))
-        rec.gmax=g1
+        rec.gmax=random.gauss(g1, g1 / 10)
         h.setpointer(diff._ref_glu, 'glu_m', rec)        # vc = h.IClamp(0.5, sec=cell.axon1.node[5])
         recs.append(rec)
         rec2 = h.nmda_rec(sec(0.5))
-        rec2.gmax=g2
+        rec2.gmax=random.gauss(g2, g2 / 10)
+        print(g2)
         h.setpointer(diff._ref_glu, 'glu_m', rec2)        # vc = h.IClamp(0.5, sec=cell.axon1.node[5])
         recs.append(rec2)
 
@@ -132,7 +167,13 @@ def show_output(v_vec, t_vec, label_v):
     t_vec: h.Vector()
         recorded time
     '''
-    pyplot.plot(t_vec, v_vec, label = label_v)
+    outavg = []
+    for i in v_vec:
+        outavg.append(list(i))
+        # pyplot.plot(t_vec, i, label = label_v)
+    outavg = np.mean(np.array(outavg), axis = 0, dtype=np.float32)
+
+    pyplot.plot(t_vec, outavg, label = label_v)
     # pyplot.plot(v_vec, v_vec13, label = 'h')
     # pyplot.plot(v_vec, v_vec16, label = 's')
     # pyplot.plot(v_vec, v_vec17, label = 'r')
@@ -160,77 +201,91 @@ if __name__ == '__main__':
     # else:
     # for i in range(5):
     # diffs = []
-    a_delta = adelta2(10, True)
-    c_fiber = cfiber(250, 0.25, 0, 15020, True, 8)
+    IN2s = []
+    IN1s = []
+    LCNs = []
+    WDRs = []
+    HTs = []
+    a_deltas = []
+    c_fibers = []
 
-    # diff = h.GrC_Gludif3(a_delta.axon1.node[0](0.5))
-    # h.setpointer(a_delta.axon1.node[5](0.5)._ref_v, 'affv', diff)
-    # diffs.append(diff)
+
+    for i in range(10):
+        IN2s.append(interneuron())
+        IN1s.append(interneuron())
+        LCNs.append(interneuron())
+        WDRs.append(WDR_model())
+        HTs.append(HT_model())
+        a_deltas.append(adelta2(10, True))
+        c_fibers.append(cfiber(250, 0.25, 0, 15000, True, 13))
     #
-    # diff2 = h.GrC_Gludif3(c_fiber.branch(0.5))
-    # h.setpointer(c_fiber.branch(0.5)._ref_v, 'affv', diff2)
-    # diffs.append(diff2)
+    #
+    # # add_glu_receptors(IN2.dend, a_delta.axon1.node[0], 10, 1)
+    for i in range(15):
+        add_glu_receptors(HTs[random.randint(0, len(HTs)-1)].dend, a_deltas[random.randint(0, len(a_deltas)-1)].axon2.node[5], 150, 300)
+        add_glu_receptors(IN2s[random.randint(0, len(IN2s)-1)].dend, a_deltas[random.randint(0, len(a_deltas)-1)].axon3.node[5], 15, 10)
+        add_glu_receptors(IN1s[random.randint(0, len(IN2s)-1)].dend, c_fibers[random.randint(0, len(a_deltas)-1)].branch, 15, 10)
+        add_glu_receptors(LCNs[random.randint(0, len(LCNs)-1)].dend, c_fibers[random.randint(0, len(c_fibers)-1)].branch, 150, 30)
+        add_glu_receptors(WDRs[random.randint(0, len(WDRs)-1)].dend, LCNs[random.randint(0, len(LCNs)-1)].axon, 150, 100)
+        add_gaba_receptors(HTs[random.randint(0, len(HTs)-1)].synlistinh, IN2s[random.randint(0, len(IN2s)-1)].axon, 0.005)
+        add_gaba_receptors(WDRs[random.randint(0, len(WDRs)-1)].synlistinh, IN2s[random.randint(0, len(IN2s)-1)].axon, 0.005)
+        # add_glu_receptors(IN1.dend, c_fiber.branch, 10, 1)
+        add_gaba_receptors(LCNs[random.randint(0, len(LCNs)-1)].synlistinh, IN1s[random.randint(0, len(IN1s)-1)].axon, 0.0025)
+        add_gaba_receptors(c_fibers[random.randint(0, len(c_fibers)-1)].synlistinh, IN1s[random.randint(0, len(IN1s)-1)].axon, 0.0035)
 
-    IN1 = interneuron()
-    IN2 = interneuron()
-    LCN = interneuron()
-    WDR =  WDR_model()
-    HT = HT_model()
 
-    add_glu_receptors(IN2.dend, a_delta.axon1.node[0], 150, 100)
-    add_glu_receptors(HT.dend, a_delta.axon1.node[0], 150, 100)
-    add_glu_receptors(LCN.dend, c_fiber.branch, 150, 100)
-    add_glu_receptors(WDR.dend, LCN.axon, 150, 100)
+    # add_5HT_receptors(IN2s[random.randint(0, len(IN2s)-1)].dend, 100, 50)
+    # add_5HT_receptors(IN1s[random.randint(0, len(IN1s)-1)].dend, 100, 50)
+    #
+    # add_5HT_receptors(a_deltas[random.randint(0, len(a_deltas)-1)].axon1.node, 100, 10)
+    # add_5HT_receptors(c_fibers[random.randint(0, len(c_fibers)-1)].stimsec, 100, 20)
 
-    # recs =[]
-    # for sec in cell2.dend:
-    #     rec = h.ampa_rec(sec(0.5))
-    #     rec.gmax=15000
-    #     h.setpointer(diff._ref_glu, 'glu_m', rec)        # vc = h.IClamp(0.5, sec=cell.axon1.node[5])
-    #     recs.append(rec)
-    #     rec2 = h.nmda_rec(sec(0.5))
-    #     rec2.gmax=100
-    #     h.setpointer(diff._ref_glu, 'glu_m', rec2)        # vc = h.IClamp(0.5, sec=cell.axon1.node[5])
-    #     recs.append(rec2)
-        # vc.delay = 10.0
-        # vc.dur = 80.0
-        # vc.dur[2] = 0.0
-        # vc.amp[0] = -70
-        # vc.amp = 0.471 + i*0.005
-        # vc.amp[2] = -45
-        # for sec in cell.all:
-        #
-        #     vc = h.VClamp(sec(0.5))
-        #     vc.dur[0] = 100.0
-        #     vc.dur[1] = 1000.0
-        #     vc.dur[2] = 1.0
-        #     vc.amp[0] = -150
-        #     vc.amp[1] = -10
-        #     vc.amp[2] = 0
+    # for sec in h.allsec():
+    #     h.psection(sec=sec) #show parameters of each section
+    v_vecs = []
+    v_vecs_cf = []
+    v_vecs_in2 = []
+    v_vecs_in1 = []
+    v_vecs_lcn = []
+    v_vecs_wdr = []
+    v_vecs_ht = []
+    wdr_spikes = []
 
-    for sec in h.allsec():
-        h.psection(sec=sec) #show parameters of each section
-    v_vec, t_vec = set_recording_vectors(a_delta.axon1.node[5])
-    v_vec_cf, t_vec = set_recording_vectors(c_fiber.branch)
-    v_vec_in2, t_vec = set_recording_vectors(IN2.soma)
-    v_vec_lcn, t_vec = set_recording_vectors(LCN.soma)
-    v_vec_wdr, t_vec = set_recording_vectors(WDR.soma)
-    v_vec_ht, t_vec = set_recording_vectors(HT.soma)
+    for i in range(0, len(c_fibers)):
+        v_vec, t_vec = set_recording_vectors(a_deltas[i].axon1.node[5])
+        v_vec_cf, t_vec = set_recording_vectors(c_fibers[i].branch)
+        v_vecs.append(v_vec)
+        v_vecs_cf.append(v_vec_cf)
+        v_vec_in2, t_vec = set_recording_vectors(IN2s[i].soma)
+        v_vec_in1, t_vec = set_recording_vectors(IN1s[i].soma)
 
-    # print("Number of model - ",cell.numofmodel)
-    # v_vec11, v_vec13, v_vec16, v_vec17, v_vec18, v_vecka, v_veckd, v_vec2, v_veckca, t_vec = set_recording_vectors(cell2.soma)
-    # v_vec11, v_vec13, v_vec16, v_vec17, v_vec18, v_vecka, v_veckd, v_vec3, v_veckca, t_vec = set_recording_vectors(cell.axon2.node[0])
+        v_vec_lcn, t_vec = set_recording_vectors(LCNs[i].soma)
+        v_vec_wdr, t_vec = set_recording_vectors(WDRs[i].soma)
+        wdr_spike_vec = spike_time_rec(WDRs[i].soma)
+        wdr_spikes.append(wdr_spike_vec)
 
-    simulate(c_fiber)
-    show_output(v_vec, t_vec, "a_delta")
-    show_output(v_vec_cf, t_vec, "c_fiber")
-    show_output(v_vec_in2, t_vec, "IN2")
-    show_output(v_vec_lcn, t_vec, "LCN")
-    show_output(v_vec_wdr, t_vec, "WDR")
-    show_output(v_vec_ht, t_vec, "HT")
+        v_vec_ht, t_vec = set_recording_vectors(HTs[i].soma)
 
-    # show_output(v_vec11, v_vec13, v_vec16, v_vec17, v_vec18, v_vecka, v_veckd, v_vec2, v_veckca, t_vec)
-    # show_output(v_vec11, v_vec13, v_vec16, v_vec17, v_vec18, v_vecka, v_veckd, v_vec3, v_veckca, t_vec)
+        v_vecs_in2.append(v_vec_in2)
+        v_vecs_in1.append(v_vec_in1)
+        v_vecs_lcn.append(v_vec_lcn)
+        v_vecs_wdr.append(v_vec_wdr)
+        v_vecs_ht.append(v_vec_ht)
+
+
+    simulate(c_fibers)
+    show_output(v_vecs, t_vec, "a_delta")
+    show_output(v_vecs_cf, t_vec, "c_fiber")
+    show_output(v_vecs_in2, t_vec, "IN2")
+
+    show_output(v_vecs_lcn, t_vec, "LCN")
+    show_output(v_vecs_wdr, t_vec, "WDR")
+    show_output(v_vecs_ht, t_vec, "HT")
+    show_output(v_vecs_in1, t_vec, "IN1")
+
+    for spike in wdr_spikes:
+        print(list(spike))
+
     pyplot.show()
 
         # pyplot.show()
